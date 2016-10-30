4140747113e90fbb9c47faaaa2f9fdacde0fbe75
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-10-30 17:48:48 -0400

    add keyboard letters to buttons

--- src/App.js
+++ src/App.js
@@ -15,19 +15,7 @@ class App extends Component {
   }
 
   keyPressHandler(event){
-    if (this.state.scale === "low"){
-      if (event.charCode === 32){
-        this.setState({scale: "high"})
-      } else {
-        $("."+ event.code).click()
-      }
-    } else {
-      if (event.charCode === 32){
-        this.setState({scale: "low"})
-      } else {
-        $("."+ event.code).click()
-      }
-    }
+    $("."+ event.code).click()
   }
 
   keyDownHandler(event){
@@ -35,7 +23,6 @@ class App extends Component {
   }
 
   keyUpHandler(event){
-    debugger;
     $("." + event.code).css("backgroundColor", "#18C964")
   }
 
@@ -58,32 +45,32 @@ class App extends Component {
     return (
       <div className="App">
         <div className="first-set">
-          <SoundPad name="Work It" classname='KeyQ' clipPath="./sounds/work_it.mp3"/>
-          <SoundPad name="Make It" classname='KeyW' clipPath="./sounds/make_it.mp3"/>
-          <SoundPad name="Do It" classname='KeyE' clipPath="./sounds/do_it.mp3"/>
-          <SoundPad name="Makes Us" classname='KeyR' clipPath="./sounds/makes_us.mp3"/>
+          <SoundPad name="Work It" keyboard="q" classname='KeyQ' clipPath="./sounds/work_it.mp3"/>
+          <SoundPad name="Make It" keyboard="w" classname='KeyW' clipPath="./sounds/make_it.mp3"/>
+          <SoundPad name="Do It" keyboard="e" classname='KeyE' clipPath="./sounds/do_it.mp3"/>
+          <SoundPad name="Makes Us" keyboard="r" classname='KeyR' clipPath="./sounds/makes_us.mp3"/>
         </div>
         <div className="second-set">
-          <SoundPad name="Harder" classname='KeyA' clipPath="./sounds/harder.mp3"/>
-          <SoundPad name="Better" classname='KeyS' clipPath="./sounds/better.mp3"/>
-          <SoundPad name="Faster" classname='KeyD' clipPath="./sounds/faster.mp3"/>
-          <SoundPad name="Stronger" classname='KeyF' clipPath="./sounds/stronger.mp3"/>
+          <SoundPad name="Harder" keyboard="a" classname='KeyA' clipPath="./sounds/harder.mp3"/>
+          <SoundPad name="Better" keyboard="s" classname='KeyS' clipPath="./sounds/better.mp3"/>
+          <SoundPad name="Faster" keyboard="d" classname='KeyD' clipPath="./sounds/faster.mp3"/>
+          <SoundPad name="Stronger" keyboard="f" classname='KeyF' clipPath="./sounds/stronger.mp3"/>
         </div>
         <div className="third-set">
-          <SoundPad name="More than" classname='KeyU' clipPath="./sounds/more_than.mp3"/>
-          <SoundPad name="Hour" classname='KeyI' clipPath="./sounds/our.mp3"/>
-          <SoundPad name="Our" classname='KeyO' clipPath="./sounds/hour.mp3"/>
-          <SoundPad name="Never" classname='KeyP' clipPath="./sounds/never.mp3"/>
+          <SoundPad name="More than" keyboard="u" classname='KeyU' clipPath="./sounds/more_than.mp3"/>
+          <SoundPad name="Hour" keyboard="i" classname='KeyI' clipPath="./sounds/our.mp3"/>
+          <SoundPad name="Our" keyboard="o"classname='KeyO' clipPath="./sounds/hour.mp3"/>
+          <SoundPad name="Never" keyboard="p" classname='KeyP' clipPath="./sounds/never.mp3"/>
         </div>
         <div className="fourth-set">
-          <SoundPad name="Ever" classname='KeyJ' clipPath="./sounds/ever.mp3"/>
-          <SoundPad name="After" classname='KeyK' clipPath="./sounds/after.mp3"/>
-          <SoundPad name="Work is" classname='KeyL' clipPath="./sounds/work_is.mp3"/>
-          <SoundPad name="Over" classname='Semicolon' clipPath="./sounds/over.mp3"/>
+          <SoundPad name="Ever" keyboard="j" classname='KeyJ' clipPath="./sounds/ever.mp3"/>
+          <SoundPad name="After" keyboard="k" classname='KeyK' clipPath="./sounds/after.mp3"/>
+          <SoundPad name="Work is" keyboard="l" classname='KeyL' clipPath="./sounds/work_is.mp3"/>
+          <SoundPad name="Over" keyboard=";" classname='Semicolon' clipPath="./sounds/over.mp3"/>
         </div>
 
         <div className="mod-key">
-          <button name="High/Low" onClick={this.toggleScale} key="Space">Go High</button>
+          <button name="High/Low" className="Space" onClick={this.toggleScale}>Go High <br/><br/><div className="key-text">Spacebar</div></button>
         </div>
       </div>
     );
@@ -91,32 +78,32 @@ class App extends Component {
     return(
       <div className="App">
         <div className="first-set">
-          <SoundPad name="Work It" classname='KeyQ' clipPath="./sounds/high/work_it.mp3"/>
-          <SoundPad name="Make It" classname='KeyW' clipPath="./sounds/high/make_it.mp3"/>
-          <SoundPad name="Do It" classname='KeyE' clipPath="./sounds/high/do_it.mp3"/>
-          <SoundPad name="Makes Us" classname='KeyR' clipPath="./sounds/high/makes_us.mp3"/>
+          <SoundPad name="Work It" keyboard="q" classname='KeyQ' clipPath="./sounds/high/work_it.mp3"/>
+          <SoundPad name="Make It" keyboard="w" classname='KeyW' clipPath="./sounds/high/make_it.mp3"/>
+          <SoundPad name="Do It" keyboard="e" classname='KeyE' clipPath="./sounds/high/do_it.mp3"/>
+          <SoundPad name="Makes Us" keyboard="r" classname='KeyR' clipPath="./sounds/high/makes_us.mp3"/>
         </div>
         <div className="second-set">
-          <SoundPad name="Harder" classname='KeyA' clipPath="./sounds/high/harder.mp3"/>
-          <SoundPad name="Better" classname='KeyS' clipPath="./sounds/high/better.mp3"/>
-          <SoundPad name="Faster" classname='KeyD' clipPath="./sounds/high/faster.mp3"/>
-          <SoundPad name="Stronger" classname='KeyF' clipPath="./sounds/high/stronger.mp3"/>
+          <SoundPad name="Harder" keyboard="a" classname='KeyA' clipPath="./sounds/high/harder.mp3"/>
+          <SoundPad name="Better" keyboard="s" classname='KeyS' clipPath="./sounds/high/better.mp3"/>
+          <SoundPad name="Faster" keyboard="d" classname='KeyD' clipPath="./sounds/high/faster.mp3"/>
+          <SoundPad name="Stronger" keyboard="f" classname='KeyF' clipPath="./sounds/high/stronger.mp3"/>
         </div>
         <div className="third-set">
-          <SoundPad name="More than" classname='KeyU' clipPath="./sounds/high/more_than.mp3"/>
-          <SoundPad name="Hour" classname='KeyI' clipPath="./sounds/high/hour.mp3"/>
-          <SoundPad name="Our" classname='KeyO' clipPath="./sounds/high/our.mp3"/>
-          <SoundPad name="Never" classname='KeyP' clipPath="./sounds/high/never.mp3"/>
+          <SoundPad name="More than" keyboard="u" classname='KeyU' clipPath="./sounds/high/more_than.mp3"/>
+          <SoundPad name="Hour" keyboard="i" classname='KeyI' clipPath="./sounds/high/hour.mp3"/>
+          <SoundPad name="Our" keyboard="o" classname='KeyO' clipPath="./sounds/high/our.mp3"/>
+          <SoundPad name="Never" keyboard="p" classname='KeyP' clipPath="./sounds/high/never.mp3"/>
         </div>
         <div className="fourth-set">
-          <SoundPad name="Ever" classname='KeyJ' clipPath="./sounds/high/ever.mp3"/>
-          <SoundPad name="After" classname='KeyK' clipPath="./sounds/high/after.mp3"/>
-          <SoundPad name="Work is" classname='KeyL' clipPath="./sounds/high/work_is.mp3"/>
-          <SoundPad name="Over" classname='Semicolon' clipPath="./sounds/high/over.mp3"/>
+          <SoundPad name="Ever" keyboard="j" classname='KeyJ' clipPath="./sounds/high/ever.mp3"/>
+          <SoundPad name="After" keyboard="k" classname='KeyK' clipPath="./sounds/high/after.mp3"/>
+          <SoundPad name="Work is" keyboard="l" classname='KeyL' clipPath="./sounds/high/work_is.mp3"/>
+          <SoundPad name="Over" keyboard=";" classname='Semicolon' clipPath="./sounds/high/over.mp3"/>
         </div>
 
         <div className="mod-key">
-          <button name="High/Low" onClick={this.toggleScale} key="Space"> Go Low</button>
+          <button name="High/Low" className="Space" onClick={this.toggleScale}> Go Low <br/><br/> <div className="key-text">Spacebar</div></button>
         </div>
       </div>
     )
